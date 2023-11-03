27e12e546e79e7318d8a99d2beddb4673938c233
goldspide <raoulbonsso@gmail.com>
2023-11-03 04:13:51 +0100

    form completion

--- lib/Screens/add.dart
+++ lib/Screens/add.dart
@@ -8,14 +8,35 @@ class Add_Screen extends StatefulWidget {
 }
 
 class _Add_ScreenState extends State<Add_Screen> {
+  DateTime date = DateTime.now();
   String? selectedItems;
+  String? selectedItems2;
+  final TextEditingController explain_C = TextEditingController();
+  FocusNode ex = FocusNode();
+  final TextEditingController amount_C = TextEditingController();
+  FocusNode amount_ = FocusNode();
   final List<String> _item = [
     'food',
     'Transfer',
     'Transportation',
     'Education'
+  ];  final List<String> _item2 = [
+    'Transportation',
+    'Education'
   ];
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ex.addListener(() {
+      setState(() {});
+    });
+
+    amount_.addListener(() {
+      setState(() {});
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
@@ -48,13 +69,193 @@ class _Add_ScreenState extends State<Add_Screen> {
           name(),
           const SizedBox(
             height: 30,
-          )
+          ),
           // TextField implementation
+          explain(),
+          const SizedBox(
+            height: 30,
+          ),
+          amount(),
+          const SizedBox(
+            height: 30,
+          ),
+          how(),
+          const SizedBox(
+            height: 30,
+          ),
+          date_time(),
+          const SizedBox(height: 30,),
+          const Spacer(),
+          save(),
+          const SizedBox(height: 20,)
+
+
         ],
       ),
     );
   }
-  Padding name(){
+
+  GestureDetector save(){
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: const Color(0xff368983),
+        ),
+        width: 120,
+        height: 50,
+        child: const Text('Save', style: TextStyle(
+          fontFamily: 'f',
+          fontWeight: FontWeight.w600,
+          color: Colors.white,
+          fontSize: 17,
+        ),),
+      ),
+    );
+  }
+
+  Widget date_time(){
+    return Container(
+      alignment: Alignment.bottomLeft,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          width: 2, color: const Color(0xffC5C5C5),
+        ),
+      ),
+      width: 300,
+      child: TextButton(onPressed: () async{
+        DateTime? newDate = await showDatePicker(context: context, initialDate: date, firstDate: DateTime(2022), lastDate: DateTime(2025),);
+        if (newDate == null) return;
+        setState(() {
+          date = newDate!;
+        });
+      }, child: Text('Date : ${date.year} /${date.month} /${date.day}' ,style: const TextStyle(
+          fontSize: 15,
+          color: Colors.black
+      ),),),
+    );
+  }
+
+  Padding how() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        width: 300,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(width: 2, color: const Color(0xffC5C5C5))),
+        child: DropdownButton<String>(
+          value: selectedItems2,
+          items: _item2
+              .map((e) => DropdownMenuItem(
+                    value: e,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 40,
+                            child: Image.asset('images/$e.png'),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            e,
+                            style: const TextStyle(fontSize: 18),
+                          )
+                        ],
+                      ),
+                    ),
+                  ))
+              .toList(),
+          selectedItemBuilder: (BuildContext context) => _item2
+              .map((e) => Row(
+                    children: [
+                      SizedBox(
+                        width: 42,
+                        child: Image.asset('images/$e.png'),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(e),
+                    ],
+                  ))
+              .toList(),
+          hint: const Text(
+            'how',
+            style: TextStyle(color: Colors.grey),
+          ),
+          dropdownColor: Colors.white,
+          isExpanded: true,
+          underline: Container(),
+          onChanged: ((value) {
+            setState(
+              () {
+                selectedItems2 = value!;
+              },
+            );
+          }),
+        ),
+      ),
+    );
+  }
+
+  Padding explain() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: TextField(
+        focusNode: ex,
+        controller: explain_C,
+        decoration: InputDecoration(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          labelText: 'explain',
+          labelStyle: TextStyle(fontSize: 17, color: Colors.grey.shade500),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(width: 2, color: Color(0xffC5C5C5)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(width: 2, color: Color(0xffC5C5C5)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding amount() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: TextField(
+        keyboardType: TextInputType.number,
+        focusNode: amount_,
+        controller: amount_C,
+        decoration: InputDecoration(
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          labelText: 'amount',
+          labelStyle: TextStyle(fontSize: 17, color: Colors.grey.shade500),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(width: 2, color: Color(0xffC5C5C5)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: const BorderSide(width: 2, color: Color(0xffC5C5C5)),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding name() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Container(
@@ -67,37 +268,51 @@ class _Add_ScreenState extends State<Add_Screen> {
           value: selectedItems,
           items: _item
               .map((e) => DropdownMenuItem(
-            value: e,
-            child: Container(
-              alignment: Alignment.center,
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: 40,
-                    child: Image.asset('images/$e.png'),
-                  ),
-                  const SizedBox(width: 10,),
-                  Text(e,style: const TextStyle(fontSize: 18),)
-                ],
-              ),
-            ),
-          ))
+                    value: e,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 40,
+                            child: Image.asset('images/$e.png'),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            e,
+                            style: const TextStyle(fontSize: 18),
+                          )
+                        ],
+                      ),
+                    ),
+                  ))
               .toList(),
-          selectedItemBuilder: (BuildContext context )=> _item.map((e) => Row(children: [
-            SizedBox(
-              width: 42,
-              child: Image.asset('images/$e.png'),
-            ),
-            const SizedBox(width: 5,),
-            Text(e),
-          ],)).toList(),
-          hint: const Text('Name',style: TextStyle(color: Colors.grey),),
+          selectedItemBuilder: (BuildContext context) => _item
+              .map((e) => Row(
+                    children: [
+                      SizedBox(
+                        width: 42,
+                        child: Image.asset('images/$e.png'),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(e),
+                    ],
+                  ))
+              .toList(),
+          hint: const Text(
+            'Name',
+            style: TextStyle(color: Colors.grey),
+          ),
           dropdownColor: Colors.white,
           isExpanded: true,
           underline: Container(),
           onChanged: ((value) {
             setState(
-                  () {
+              () {
                 selectedItems = value!;
               },
             );
